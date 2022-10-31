b9fc380f4de0895bc6e2ff9b3cc9df5f7b5ba584
deen <deenpsut@gmail.com>
2022-10-31 18:13:07 +0200

    assignment 1

--- app/src/main/java/com/example/scribble/activity_start.kt
+++ app/src/main/java/com/example/scribble/activity_start.kt
@@ -1,15 +1,9 @@
 package com.example.scribble
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextUtils
-import android.text.TextWatcher
 import android.view.View
 import android.widget.*
-import androidx.core.widget.doOnTextChanged
-import com.google.android.material.navigation.NavigationBarView
+
 
 class activity_start : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +12,7 @@ class activity_start : AppCompatActivity() {
         var textview_fact: TextView = findViewById(R.id.textview_fact)
         var flag : String = "funny"
         val spinnerVal : Spinner = findViewById(R.id.spinner_categories)
-        var options = arrayOf("select category here","funny","psychology","animals","physics")
+        val options = arrayOf("select category here","funny","psychology","animals","physics")
         spinnerVal.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,options )
         spinnerVal.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
@@ -41,20 +35,20 @@ class activity_start : AppCompatActivity() {
 }
 public fun getFact(str: String): String {
     var fact: String = "ready to learn a fact?"
-    var funny = arrayOf("A man once wore 60 shirts and 9 pairs of jeans to the airport to avoid extra fees",
+    val funny = arrayOf("A man once wore 60 shirts and 9 pairs of jeans to the airport to avoid extra fees",
         "cows produce 3% less milk when listening to country music",
        " The CDC website has a zombie preparedness section"
     )
     //https://facts.net/funny-facts/
     //https://stacks.cdc.gov/view/cdc/6023
-    var psychology = arrayOf("your brain can store an estimated 2,500,000 gigabytes of info!",
+    val psychology = arrayOf("Your brain can store 2,500,000 gigabytes of info!",
         "Sweating can temporarily shrink the brain.",
         "The brain generates between 12 and 25 watts of electricity—that’s enough to power a low wattage light bulb!")
     //https://legacybox.com/blogs/analog/25-brain-facts-blow-mind
-    var animals = arrayOf("Turkeys have a vision of 270°.",
+    val animals = arrayOf("Turkeys have a vision of 270°.",
         "Dalmatians are born without spots!","Polar bear skin is black!")
     //https://animalmedical.org/fun-facts
-    var physics = arrayOf("Physics originated from astronomy, mathematics, and natural philosophy in 3000 B.C.",
+    val physics = arrayOf("Physics originated from astronomy, mathematics, and natural philosophy in 3000 B.C.",
         "Thales was the first physicist. He believed the world was only built from one element: water",
         "Newton formed the Laws of Motion in 1687.\n"
     )
